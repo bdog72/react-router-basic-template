2dc517580068352e4d91a3284681c680347344f6
Brian Bycynski <high5bri@hotmail.com>
2017-07-14 08:58:42 -0400

    basic react router example

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-
+import ReactRouter from './Router0/ReactRouter'
 class App extends Component {
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      <ReactRouter />
     </div>
   }
 }
---
+++ src/components/Router0/ReactRouter.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import Home from './Home'
+import About from './About'
+import Topics from './Topics'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+export default class ReactRouter1 extends Component {
+  render () {
+    return <div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/topics'>Topics</Link></li>
+          </ul>
+          <hr />
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/topics' component={Topics} />
+        </div>
+      </Router>
+    </div>
+  }
+}
